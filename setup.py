80badfb78d1a11213aec890e5258310bcfffde3c
Mike Sumner <sumnermichael@hotmail.com>
2021-07-23 16:31:07 +0100

    updating setup

--- setup.py
+++ setup.py
@@ -1,5 +1,13 @@
 from setuptools import setup
 
+requires = [
+    'certifi == 2021.5.30',
+    'charset - normalizer == 2.0.3',
+    'idna == 3.2',
+    'requests == 2.26.0',
+    'urllib3 == 1.26.6',
+]
+
 setup(
     name='allianrt api',
     version='0.1',
@@ -8,5 +16,7 @@ setup(
     license='',
     author='msumner',
     author_email='msumner@realsoftwaresystems.com',
-    description='A library for interacting with RSS\'s Alliant software API'
+    description='A library for interacting with RSS\'s Alliant software API',
+    python_requires=">=3.8",
+    install_requires=requires,
 )
