59940d2d77810e2c5b57b2fdad346af67cd81dcc
Mike Sumner <sumnermichael@hotmail.com>
2021-07-28 10:45:03 +0100

    refactored subclasses

--- alliantapi/alliant_api_response.py
+++ alliantapi/alliant_api_response.py
@@ -25,11 +25,11 @@ class AlliantApiResponse(Response):
         self.__setstate__(response.__getstate__())
 
         try:
-            self.errors = self.json().get('errors')
-            self.has_errors = self.json().get('hasErrors')
+            self.errors: list = self.json().get('errors')
+            self.has_errors: bool = self.json().get('hasErrors')
             self.result = self.json().get('result')
-            self.warnings = self.json().get('warnings')
-            self.has_warnings = self.json().get('hasWarnings')
+            self.warnings: list = self.json().get('warnings')
+            self.has_warnings: bool = self.json().get('hasWarnings')
 
         except JSONDecodeError:
             logging.error(
@@ -65,29 +65,15 @@ class AlliantApiResponse(Response):
 
 class Collection(AlliantApiResponse):
 
-    @property
-    def next_page_url(self) -> str:
-        return self.result.get('previousPageUrl')
+    def __init__(self, response: Response):
+        super().__init__(response)
 
-    @property
-    def previous_page_url(self) -> str:
-        return self.result.get('nextPageUrl')
-
-    @property
-    def items(self) -> list:
-        return self.result.get('items')
-
-    @property
-    def item_count(self) -> int:
-        return self.result.get('itemCount')
-
-    @property
-    def total_item_count(self) -> int:
-        return self.result.get('totalItemCount')
-
-    @property
-    def guids(self) -> list:
-        return [item.get('guid') for item in self.items]
+        self.next_page_url: str = self.result.get('previousPageUrl')
+        self.previous_page_url: str = self.result.get('nextPageUrl')
+        self.items: list = self.result.get('items')
+        self.item_count: int = self.result.get('itemCount')
+        self.total_item_count: int = self.result.get('totalItemCount')
+        self.guids: list = [item.get('guid') for item in self.items]
 
 
 class Adjustment(AlliantApiResponse):
@@ -95,16 +81,13 @@ class Adjustment(AlliantApiResponse):
     A subclass of Response.  This offers additional methods to return data specific to the adjustment header object.
     It can only works with a resource and not with a collection
     """
-
-    @property
-    def adjustment_status(self):
+    def __init__(self, response: Response):
+        super().__init__(response)
 
         try:
-            status = self.result.get('statusReference').get('displayName')
+            self.adjustment_status: str = self.result.get('statusReference').get('displayName')
         except AttributeError:
-            status = None
-
-        return status
+            self.adjustment_status = None
 
 
 class Contract(AlliantApiResponse):
@@ -113,12 +96,11 @@ class Contract(AlliantApiResponse):
     It can only works with a resource and not with a collection
     """
 
-    @property
-    def contract_status(self):
+    def __init__(self, response: Response):
+        super().__init__(response)
 
         try:
-            status = self.result.get('statusReference').get('displayName')
+            self.contract_status: str = self.result.get('statusReference').get('displayName')
         except AttributeError:
-            status = None
+            self.contract_status = None
 
-        return status
