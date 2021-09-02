e017355e87f3441740db82ea35b0808c720c468d
Mike Sumner <sumnermichael@hotmail.com>
2021-09-02 17:59:13 +0100

    typo

--- alliantapi/__init__.py
+++ alliantapi/__init__.py
@@ -6,9 +6,9 @@ response and if you are familiar with Requests, then it should feel familiar.
 
 This library will contain some methods that interact directly with the API endpoints, as well as higher level methods
 that apply logic and execute across multiple endpoints to achieve specific tasks. For anyone contributing to
-development, The low level access methods exist in the Client class, located in client.py. The AlliantAPI class is a
+development, The low level access methods exist in the Client class, located in client.py. The AlliantApi class is a
 subclass that utilises the low level methods in Client to form higher level abstractions and methods from either can be
-accessed by end users using AlliantAPI
+accessed by end users using AlliantApi
 """
 
 from .client import get_system_layers, get_application_layers
