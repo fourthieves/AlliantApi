9d8c2ab4a004fb1ec0d78f5ca156060d7cd5b144
mike.sumner <LikkLikk1!>
2021-09-06 17:19:33 +0100

    Added resource method - complete_and_approve_contract

--- alliantapi/resource.py
+++ alliantapi/resource.py
@@ -1,14 +1,23 @@
 from .client import Client, get_system_layers, get_application_layers
 from.parameters import ResourceParameters, CollectionParameters
+from .alliant_api_response import AlliantApiResponse
 import logging
 
 
 class AlliantApi(Client):
 
-    def __init__(self, base_url, user_id=None, password=None, system_layer_key=None, application_layer=None,
-                 database_server=None, database_username=None, database_password=None):
+    def __init__(self, base_url: str, user_id: str = None, password: str = None, system_layer_key: str = None,
+                 application_layer: str = None,
+                 number_of_retries: int = 3,
+                 retry_delay: int = 3,
+                 retry_backoff: int = 2,
+                 database_server: str = None,
+                 database_username: str = None,
+                 database_password: str = None
+                 ):
 
-        super().__init__(base_url, user_id, password, system_layer_key, application_layer)
+        super().__init__(base_url, user_id, password, system_layer_key, application_layer, number_of_retries,
+                         retry_delay, retry_backoff)
 
         self.database_server = database_server
         self.database_username = database_username
@@ -88,3 +97,31 @@ class AlliantApi(Client):
                 return f"{guid} - Error - {response.errors}"
 
         return f"{guid} - {response.contract_status} not deleted"
+
+    def complete_and_approve_contract(self, guid: str, approve_message: str) -> AlliantApiResponse:
+        """
+        Takes a contract through the complete and approval stages of contract lifecycle
+
+        :param guid: the guid for the resource you are referencing
+        :type guid: str
+        :param approve_message: The approval message to be used
+        :type approve_message: str
+        :return: AlliantApiResponse
+        :rtype: AlliantApiResponse
+        """
+
+        response = self.lookup_contract(guid)
+        contract_id = response.result['id']
+        logging.info(f"The initial contract status for {contract_id} is {response.contract_status}")
+
+        if response.contract_status in ('In Revision', 'In Setup'):
+            self.contract_action(guid, 'complete')
+            response = self.lookup_contract(guid)
+
+        if response.contract_status  == 'Complete':
+            response = self.contract_action(guid, 'approve', approve_message)
+            if response:
+                response = self.lookup_contract(guid)
+
+        logging.info(f"The final contract status for {contract_id} is {response.contract_status}")
+        return response
