b19f98229aa192301d7f989caea6ef43782dfb01
Mike Sumner <sumnermichael@hotmail.com>
2022-04-25 11:35:41 +0100

    handled guid lookups where list is empty

--- alliantapi/client.py
+++ alliantapi/client.py
@@ -601,7 +601,10 @@ class Client:
 
         response = self.lookup_contract_with_filter(filter_field, filter_value, verbosity='minimal')
 
-        return response.guids[0]
+        if response.guids:
+            return response.guids[0]
+        else:
+            return None
 
     def patch_contract(self, guid: str, body: dict,
                        resource_parameters: ResourceParameters = ResourceParameters(None)) -> Contract:
@@ -765,7 +768,10 @@ class Client:
 
         response = self.lookup_contact_with_filter(filter_field, filter_value, verbosity='minimal')
 
-        return response.guids[0]
+        if response.guids:
+            return response.guids[0]
+        else:
+            return None
 
     def lookup_contact(self, guid, verbosity='default') -> AlliantApiResponse:
 
